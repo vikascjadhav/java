580d9d23b09822f68eb1cb0cc9598489a230721e
Vikas Jadhav <vikascjadhav@gmail.com>
2023-01-02 13:37:22 +0530

    predicate

---
+++ pluralsight/java8/src/main/java/com/practice/java8/FirstPredicate.java
@@ -0,0 +1,22 @@
+
+
+package com.practice.java8;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class FirstPredicate {
+
+    public static void main(String[] args) {
+
+        Stream<String> stream = Stream.of("one", "two", "three", "four", "five");
+
+        Predicate<String> p1 = s -> s.length() > 3;
+        Predicate<String> p2 = Predicate.isEqual("two");
+
+        stream.filter(p1.or(p2)).forEach(System.out::println);
+
+
+
+    }
+}
